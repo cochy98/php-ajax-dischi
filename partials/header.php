84dc3446a1ca554f425f64d792831bca514a38ad
cochy98 <cosmoferrigno@gmail.com>
2022-04-22 15:48:08 +0200

    Add header.php

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,19 @@
 <header>
-  <h1>header</h1>
+  <nav class="navbar navbar-light">
+    <div class="container-fluid px-5">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo-small.svg" alt="Spotify Logo" />
+      </a>
+      <div class="d-flex">
+        <select class="form-select me-2">
+          <option value="" selected>All</option>
+          <option value="Rock">Rock</option>
+          <option value="Pop">Pop</option>
+          <option value="Jazz">Jazz</option>
+          <option value="Metal">Metal</option>
+        </select>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </div>
+    </div>
+  </nav>
 </header>
